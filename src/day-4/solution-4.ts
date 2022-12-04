4874b130bc43329aa2f2523f76e5c0e70adc541e
Martin Mederly <martinmederly@gmail.com>
2022-12-04 10:20:32 +0100

    feat: day four, part two

--- src/day-4/solution-4.ts
+++ src/day-4/solution-4.ts
@@ -11,11 +11,15 @@ const parseIdsToElfAssignment = (idInput: string[]): ElfAssignment => ({start: p
 
 const parseElfAssignmentsFromInput = (input: string[]): ElfPair[] => input.map(line => ({elves: line.split(",").map(e => parseIdsToElfAssignment(e.split("-")))}));
 
-const isAssignmentOverlapping = (a1: ElfAssignment, a2: ElfAssignment) => a1.start >= a2.start && a1.end <= a2.end;
-const isPairOverlapping = (pair: ElfPair): boolean => isAssignmentOverlapping(pair.elves[0], pair.elves[1]) || isAssignmentOverlapping(pair.elves[1], pair.elves[0])
+const isAssignmentOverlappingFully = (a1: ElfAssignment, a2: ElfAssignment) => a1.start >= a2.start && a1.end <= a2.end;
+const isPairOverlappingFully = (pair: ElfPair): boolean => isAssignmentOverlappingFully(pair.elves[0], pair.elves[1]) || isAssignmentOverlappingFully(pair.elves[1], pair.elves[0])
+
+const isAssignmentOverlappingPartially = (a1: ElfAssignment, a2: ElfAssignment) => a1.start <= a2.start && a1.end >= a2.start;
+const isPairOverlappingPartially = (pair: ElfPair): boolean => isAssignmentOverlappingPartially(pair.elves[0], pair.elves[1]) || isAssignmentOverlappingPartially(pair.elves[1], pair.elves[0])
 
 exports.solution = (input: string[]) => {
     const elfPairs = parseElfAssignmentsFromInput(input);
 
-    console.log(elfPairs.filter(ep => isPairOverlapping(ep)).length);
+    console.log(elfPairs.filter(ep => isPairOverlappingFully(ep)).length);
+    console.log(elfPairs.filter(ep => isPairOverlappingPartially(ep)).length);
 }
