54adf3c65a407324629fb0779da62a009d45d3bd
Martin Mederly <martinmederly@gmail.com>
2022-12-04 10:13:43 +0100

    feat: day four, part one

--- src/day-4/solution-4.ts
+++ src/day-4/solution-4.ts
@@ -1,3 +1,21 @@
+type ElfAssignment = {
+    start: number;
+    end: number;
+}
+
+type ElfPair = {
+    elves: ElfAssignment[],
+}
+
+const parseIdsToElfAssignment = (idInput: string[]): ElfAssignment => ({start: parseInt(idInput[0]), end: parseInt(idInput[1])})
+
+const parseElfAssignmentsFromInput = (input: string[]): ElfPair[] => input.map(line => ({elves: line.split(",").map(e => parseIdsToElfAssignment(e.split("-")))}));
+
+const isAssignmentOverlapping = (a1: ElfAssignment, a2: ElfAssignment) => a1.start >= a2.start && a1.end <= a2.end;
+const isPairOverlapping = (pair: ElfPair): boolean => isAssignmentOverlapping(pair.elves[0], pair.elves[1]) || isAssignmentOverlapping(pair.elves[1], pair.elves[0])
+
 exports.solution = (input: string[]) => {
-    console.log("TODO: solution");
+    const elfPairs = parseElfAssignmentsFromInput(input);
+
+    console.log(elfPairs.filter(ep => isPairOverlapping(ep)).length);
 }
