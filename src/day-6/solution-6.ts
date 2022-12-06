43dc1dd79492dafcb9db38ef656953e8ca04d9e3
Medo <martinmederly@gmail.com>
2022-12-06 19:46:33 +0100

    feat: day six, second solution = using sets (and not alphabet-mapping object like before)

--- src/day-6/solution-6.ts
+++ src/day-6/solution-6.ts
@@ -1,5 +1,6 @@
 
-const findMarker = (l: string, len: number): number => {
+/* This was first, using mapping object for characters */
+const findMarker_v1 = (l: string, len: number): number => {
     for (let i = len - 1; i < l.length; i++) {
 
         const arr: Record<string, boolean> = {}
@@ -19,9 +20,20 @@ const findMarker = (l: string, len: number): number => {
     return 0;
 }
 
+/* This is second, a lot cleaner, using sets */
+const findMarker_v2 = (l: string, len: number): number => {
+    for (let i = 0; i < l.length - len; i++) {
+        const unique = new Set(l.slice(i, i+len));
+        if (unique.size === len) return i + len;
+    }
+    return 0;
+}
+
+
+
 exports.solution = (input: string[]) => {
     const signal = input[0];
 
-    console.log(findMarker(signal, 4));
-    console.log(findMarker(signal, 14));
+    console.log(findMarker_v2(signal, 4));
+    console.log(findMarker_v2(signal, 14));
 }
