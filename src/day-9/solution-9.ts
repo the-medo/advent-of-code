35e23e26fca4424aaf9809e3deb80251e1481fe8
Medo <martinmederly@gmail.com>
2022-12-09 17:04:09 +0100

    feat: day nine, part two

--- src/day-9/solution-9.ts
+++ src/day-9/solution-9.ts
@@ -34,7 +34,7 @@ const moveHead = (h: Point, direction: Directions) => {
     h.x += DirectionalMoves[direction].x;
     h.y += DirectionalMoves[direction].y;
 }
-const moveTail = (h: Point, t: Point, v: Set<string>) => {
+const moveTail = (h: Point, t: Point, v: Set<string> | undefined) => {
     if (!arePointsTouching(h, t)) {
         if (h.x > t.x) t.x += 1;
         if (h.x < t.x) t.x -= 1;
@@ -42,18 +42,13 @@ const moveTail = (h: Point, t: Point, v: Set<string>) => {
         if (h.y < t.y) t.y -= 1;
     }
 
-    v.add(`${t.x}_${t.y}`);
+    if (v) v.add(`${t.x}_${t.y}`);
 };
 
 exports.solution = (input: string[]) => {
-    const head: Point = {
-        x: 0,
-        y: 0,
-    };
-    const tail: Point = {
-        x: 0,
-        y: 0,
-    };
+    const pointCount = 10;
+    const points: Point[] = []
+    for (let i = 0; i < pointCount; i++) points.push({ x: 0, y: 0, });
 
     const visitedByTail = new Set<string>;
 
@@ -63,12 +58,12 @@ exports.solution = (input: string[]) => {
         const moveCount = parseInt(b);
 
         for (let i = 1; i <= moveCount; i++) {
-            moveHead(head, direction);
-            moveTail(head, tail, visitedByTail);
+            moveHead(points[0], direction);
+            for (let j = 1; j < pointCount; j++) {
+                moveTail(points[j-1], points[j], j === pointCount - 1 ? visitedByTail : undefined);
+            }
         }
     });
 
     console.log(visitedByTail.size);
-
-
 }
