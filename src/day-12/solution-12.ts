cfa37f0c29d2eb797528311e84b2cc63e66c6975
Martin Mederly <martinmederly@gmail.com>
2022-12-12 12:14:29 +0100

    feat: day twelve, work in progress part 1, PC change

--- src/day-12/solution-12.ts
+++ src/day-12/solution-12.ts
@@ -1,3 +1,31 @@
+type P = {
+    elevation: number;
+    neighbors: P[];
+    visited: boolean;
+}
+
+const createPoint = (char: string): P => ({
+    elevation: char.charCodeAt(0),
+    neighbors: [],
+    visited: false,
+})
+
 exports.solution = (input: string[]) => {
     console.log("TODO: solution");
+    let start: P;
+    let end: P;
+
+    const map: P[][] = input.map((l) =>  l.split("").map(x => {
+        let c = x;
+        if (x === 'S') c = 'a';
+        if (x === 'E') c = 'z';
+        const p = createPoint(c);
+        if (x === 'S') start = p;
+        if (x === 'E') end = p;
+        return p;
+    }));
+
+    console.log(map);
+    console.log(start!);
+    console.log(end!);
 }
