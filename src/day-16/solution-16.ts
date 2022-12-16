927557398f19c7ce64b57818080ca6435678578e
Martin Mederly <martinmederly@gmail.com>
2022-12-16 12:46:03 +0100

    feat: day sixteen, parsing

--- src/day-16/solution-16.ts
+++ src/day-16/solution-16.ts
@@ -1,3 +1,38 @@
+type Valve = {
+    id: string;
+    flowRate: number;
+    valves: Valve[];
+}
+
+
+
+const createValve = (id: string, flowRate: number): Valve => ({id, flowRate, valves: []})
+
 exports.solution = (input: string[]) => {
-    console.log("TODO: solution");
+    //Valve II has flow rate=0; tunnels lead to valves AA, JJ
+    const valveArray: Valve[] = [];
+    const valveRecord: Record<string, Valve> = {}
+    const connectionInfo: Record<string, string[]> = {};
+
+    input.forEach(i => {
+       const [valveId, flowRate, connections] = i.replace('Valve ', '')
+           .replace(' has flow rate=', ';')
+           .replace(' tunnels lead to valves ', '')
+           .replace(' tunnel leads to valve ', '')
+           .split(';')
+       const valve = createValve(valveId, parseInt(flowRate));
+       valveArray.push(valve);
+       valveRecord[valveId] = valve;
+       connectionInfo[valveId] = connections.split(', ');
+    });
+
+    Object.keys(connectionInfo).forEach(k => {
+        connectionInfo[k].forEach(connection => {
+            valveRecord[k].valves.push(valveRecord[connection]);
+        })
+        console.log(k, valveRecord[k].valves);
+    });
+
+    console.log(valveArray);
+    console.log(valveRecord);
 }
