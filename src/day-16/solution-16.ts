0237b582b7847f20d9952ac7f4e453806cc2f384
Medo <martinmederly@gmail.com>
2022-12-17 01:57:07 +0100

    feat: day sixteen, part two

--- src/day-16/solution-16.ts
+++ src/day-16/solution-16.ts
@@ -18,25 +18,34 @@ const maxMinute = 30;
 
 let counter = 0;
 
-const actionPart1 = (currentValve: Valve, openedValves: string[], totalFlowRate: number, minute: number): number => {
-    counter++;
-    console.log(counter, " - ", currentValve.id, openedValves, totalFlowRate, minute);
-    if (minute >= maxMinute) return totalFlowRate;
+type ValveOpenerInfo = {
+    id: number;
+    currentValve: Valve,
+    minutes: number,
+}
 
-    const computedValveScore = valveArray.map(v => {
-        if (openedValves.includes(v.id)) return {valve: v, distance: 1, score: 0};
-        const distance = 1 + valveDistances[currentValve.id][v.id];
-        return {
-            valve: v,
-            distance: distance ?? 1,
-            score: (maxMinute - (minute + distance)) * v.flowRate,
-        }
-    }).filter(v => v.score > 0)
-      .sort((a, b) => b.score - a.score);
+const copyOpeners = require('lodash/cloneDeep');
+
+const action = (openers: ValveOpenerInfo[], openedValves: string[], totalFlowRate: number): number => {
+    counter++;
+    if (counter % 10000 === 0) console.log(counter);
+
+    const sortedOpeners = (copyOpeners(openers) as ValveOpenerInfo[]).sort((a, b) => a.minutes - b.minutes);
+
+    const opener = sortedOpeners[0];
+    if (opener.minutes >= maxMinute) return totalFlowRate;
+
+    const remainingValves = valveArray.filter(v => !openedValves.includes(v.id));
+    if (remainingValves.length === 0) return totalFlowRate;
 
     const results: number[] = [];
 
-    computedValveScore.forEach(valveToOpen => results.push(actionPart1(valveToOpen.valve, [...openedValves, valveToOpen.valve.id], totalFlowRate + valveToOpen.score, minute + valveToOpen.distance)))
+    remainingValves.forEach(valve => {
+        const distance = 1 + valveDistances[opener.currentValve.id][valve.id];
+        const flow = (maxMinute - (opener.minutes + distance)) * valve.flowRate;
+        if (flow > 0) results.push(action( [...openers.filter(o => o.id !== opener.id), {id: opener.id, currentValve: valve, minutes: opener.minutes + distance}], [...openedValves, valve.id], totalFlowRate + flow  ));
+    });
+
     if(results.length > 0) return Math.max(...results);
     return totalFlowRate;
 }
@@ -45,6 +54,7 @@ const createValve = (id: string, flowRate: number): Valve => ({id, flowRate, con
 
 exports.solution = (input: string[]) => {
 
+    //Parse input, create valveArray + valveRecord + connectionInfo
     input.forEach(i => {
        const [valveId, flowRate, connections] = i.replace('Valve ', '')
            .replace(' has flow rate=', ';')
@@ -57,6 +67,7 @@ exports.solution = (input: string[]) => {
        connectionInfo[valveId] = connections.split(', ');
     });
 
+    //add connections into valveRecord
     Object.keys(connectionInfo).forEach(k => {
         connectionInfo[k].forEach(connection => {
             valveRecord[k].connections.push({distance: 1, valve: valveRecord[connection]});
@@ -64,6 +75,7 @@ exports.solution = (input: string[]) => {
         console.log(k, valveRecord[k].connections);
     });
 
+    //remove valves without flow from graph (and add their "distance" to connected connections)
     valveArray.forEach(v => {
        if (v.flowRate === 0 && v.id !== 'AA') {
            console.log("Found valve with flow 0: ", v.id);
@@ -93,8 +105,9 @@ exports.solution = (input: string[]) => {
 
     const startingValve: Valve = valveRecord['AA'];
 
-    valveArray = valveArray.filter(v => v.flowRate > 0 || v.id === startingValve.id);
+    valveArray = valveArray.filter(v => v.flowRate > 0 || v.id === startingValve.id); //remove valves without flow also from valveArray
 
+    //create "distance map" => info about distance between each 2 valves
     valveArray.forEach(v1 => {
         valveDistances[v1.id] = {};
         valveArray.forEach(v2 => valveDistances[v1.id][v2.id] = v1.id !== v2.id ? Infinity : 0)
@@ -121,6 +134,19 @@ exports.solution = (input: string[]) => {
 
     console.log(valveDistances);
 
-    const result = actionPart1(startingValve,  [startingValve.id], 0, 0);
-    console.log("Result part 1: ", result);
+    const result1 = action(
+        [{id: 1, currentValve: startingValve, minutes: 4}, {id: 2, currentValve: startingValve, minutes: 4}],
+        [startingValve.id],
+        0
+    );
+    console.log("Result part 1 (alone): ", result1);
+
+    counter = 0;
+    const result2 = action(
+        [{id: 1, currentValve: startingValve, minutes: 4}, {id: 2, currentValve: startingValve, minutes: 4}],
+        [startingValve.id],
+        0
+    );
+
+    console.log("Result part 2 (with clever elephant): ", result2);
 }
