8ac1bafd5a7cc29dc8f9285e60e1a912d02d878d
Medo <martinmederly@gmail.com>
2024-11-30 13:03:14 +0100

    feat: prepare old repo for this year

--- run.ts
+++ run.ts
@@ -1,8 +1,13 @@
 import getInput from "./src/utils/getInput";
 
 if (process.argv[2] !== undefined) {
+    let year = new Date().getFullYear();
+    if (process.argv[3] !== undefined) {
+        year = parseInt(process.argv[3]);
+    }
+
     const solutionId: number = parseInt(process.argv[2]);
-    require(`./src/day-${solutionId}/solution-${solutionId}.ts`).solution(getInput(solutionId));
+    require(`./src/${year}/day-${solutionId}/solution-${solutionId}.ts`).solution(getInput(solutionId, year));
 } else {
     throw Error("Please insert Day as first parameter");
 }
--- src/utils/getInput.ts
+++ src/utils/getInput.ts
@@ -1,4 +1,4 @@
 
-const getInput = (day: number): string[] =>  require('fs').readFileSync(`./src/day-${day}/input-${day}.txt`, 'utf8').split(/\r?\n/);
+const getInput = (day: number, year: number): string[] =>  require('fs').readFileSync(`./src/${year}/day-${day}/input-${day}.txt`, 'utf8').split(/\r?\n/);
 
 export default getInput;
