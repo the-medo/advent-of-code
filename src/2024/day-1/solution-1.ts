be136bfdce0095fc313b2492edcd51566c356fd9
Medo <martinmederly@gmail.com>
2024-12-01 06:06:13 +0100

    feat: day1 p1

--- src/2024/day-1/solution-1.ts
+++ src/2024/day-1/solution-1.ts
@@ -1,3 +1,21 @@
 exports.solution = (input: string[]) => {
-    console.log("TODO: solution");
+    let list1: number[] = [];
+    let list2: number[] = [];
+
+    const nMap = {};
+    input.forEach(i => {
+        const numbers = i.split('   ');
+        list1.push(parseInt(numbers[0]));
+        list2.push(parseInt(numbers[1]));
+
+    });
+    list1.sort((a,b) => a-b)
+    list2.sort((a,b) => a-b)
+
+    let diff=0
+    list1.forEach((n, i) => {
+        diff += Math.abs(n-list2[i]);
+    })
+
+    console.log(diff);
 }
