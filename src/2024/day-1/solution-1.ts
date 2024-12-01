a0c20cd1c3df43d273ba2a82176967cf80659c0b
Medo <martinmederly@gmail.com>
2024-12-01 06:12:23 +0100

    feat: day1 p2

--- src/2024/day-1/solution-1.ts
+++ src/2024/day-1/solution-1.ts
@@ -2,20 +2,29 @@ exports.solution = (input: string[]) => {
     let list1: number[] = [];
     let list2: number[] = [];
 
-    const nMap = {};
+    const nMap: Record<number, number | undefined> = {};
     input.forEach(i => {
         const numbers = i.split('   ');
         list1.push(parseInt(numbers[0]));
-        list2.push(parseInt(numbers[1]));
+        const rightNumber = parseInt(numbers[1]);
+        list2.push(rightNumber);
 
+        if (nMap[rightNumber] === undefined) {
+            nMap[rightNumber] = 1;
+        } else {
+            nMap[rightNumber]!++;
+        }
     });
     list1.sort((a,b) => a-b)
     list2.sort((a,b) => a-b)
 
     let diff=0
+    let diff2=0
     list1.forEach((n, i) => {
         diff += Math.abs(n-list2[i]);
+        diff2 += n * (nMap[n] ?? 0);
     })
 
     console.log(diff);
+    console.log(diff2);
 }
