4d7e111d99494d598c93b2e81690e9a07f5daf2e
Medo <martinmederly@gmail.com>
2024-12-02 07:43:57 +0100

    feat: day2 p2 (refactor)

--- src/2024/day-2/solution-2.ts
+++ src/2024/day-2/solution-2.ts
@@ -1,18 +1,21 @@
+/* returns -1 if the report is "safe", otherwise returns index of error */
 const isSafe = (r: number[]): number => {
 
     let lastValue = r[0];
     let direction = r[0] - r[r.length - 1] > 0 ? 1 : -1
     let safe = -1;
-    r.forEach((v, i) => {
+    r.find((v, i) => {
+        if (i === 0) return;
         if (
             lastValue - v > 0 && direction < 0 ||
             lastValue - v < 0 && direction > 0 ||
-            (lastValue - v === 0 && i > 0) ||
+            (lastValue - v === 0) ||
             Math.abs(lastValue - v) > 3
         ) {
             safe = i;
         }
         lastValue = v;
+        return safe >= 0
     })
 
     return safe;
@@ -21,37 +24,35 @@ const isSafe = (r: number[]): number => {
 exports.solution = (input: string[]) => {
     const reports: number[][] = input.map(i => i.split(' ').map(i => parseInt(i)));
 
-    let safeReports = 0;
+    let safeReportsPart1 = 0;
+    let safeReportsPart2 = 0;
     reports.forEach(r => {
-        console.log("==================");
         let safe = isSafe(r)
 
+        //if safe is -1, it means that report is safe
         if (safe >= 0) {
-            const array1 = [...r], array2 = [...r];
-            array1.splice(safe,1)
+            //we know what position was "unsafe", so we can try to remove element at that position (or at position before)
+            const try1 = [...r], try2 = [...r];
+            try1.splice(safe, 1)
+            const triesWithRemovedFloors = [try1]
             if (safe > 0) {
-                array2.splice(safe-1, 1);
+                try2.splice(safe-1, 1);
+                triesWithRemovedFloors.push(try2);
             }
-            const tries = [array1, array2]
-            console.log(safe, "gonna try", tries)
-            const changeIsSafe = tries.find(t => {
-                const changeSafe = isSafe(t)
-                console.log("=== change: ", changeSafe);
-                return changeSafe === -1;
-            })
+
+            //we check if at least one of these changes is safe
+            const changeIsSafe = triesWithRemovedFloors.find(t =>  isSafe(t) === -1)
+
             if (changeIsSafe !== undefined) {
                 safe = -1;
             }
+        } else {
+            safeReportsPart1++;
         }
 
-        console.log(safe);
-        if (safe === -1) safeReports++;
-        console.log("==================");
+        if (safe === -1) safeReportsPart2++;
     })
 
-    console.log(safeReports);
-
-
-
-
+    console.log("Total safe reports in part 1:",  safeReportsPart1);
+    console.log("Total safe reports in part 2:",  safeReportsPart2);
 }
