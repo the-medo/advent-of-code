87e0329de4c91658187e93c7b9858cd6b018e1ac
Medo <martinmederly@gmail.com>
2024-12-02 06:12:27 +0100

    feat: day2 p1

--- src/2024/day-2/solution-2.ts
+++ src/2024/day-2/solution-2.ts
@@ -1,3 +1,29 @@
 exports.solution = (input: string[]) => {
-    console.log("TODO: solution");
+    const reports: number[][] = input.map(i => i.split(' ').map(i => parseInt(i)));
+
+    let safeReports = 0;
+    reports.forEach(r => {
+        let lastValue = r[0];
+        let direction = r[0] - r[r.length - 1] > 0 ? 1 : -1
+        let safe = true;
+        r.forEach((v, i) => {
+            if (
+                lastValue - v > 0 && direction < 0 ||
+                lastValue - v < 0 && direction > 0 ||
+                (lastValue - v === 0 && i > 0) ||
+                Math.abs(lastValue - v) > 3
+            ) {
+                safe = false;
+            }
+            lastValue = v;
+        })
+        console.log(safe);
+        if (safe) safeReports++;
+    })
+
+    console.log(safeReports);
+
+
+
+
 }
