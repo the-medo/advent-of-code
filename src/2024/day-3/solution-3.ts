9a18933841db0220a9e022f361904098d8f97d2d
Medo <martinmederly@gmail.com>
2024-12-03 06:55:17 +0100

    feat: day3 p1

--- src/2024/day-3/solution-3.ts
+++ src/2024/day-3/solution-3.ts
@@ -1,3 +1,57 @@
 exports.solution = (input: string[]) => {
-    console.log("TODO: solution");
+    let line = input.join('');
+    const splitline = line.split('');
+
+    // let regex = /mul\([0-9]+,[0-9]+\)/g
+    // const match = regex.exec(line)
+
+    // console.log(match)
+
+
+    let index = 0
+    let totalResult = 0;
+    let occurences = 0;
+
+    while (index > -1) {
+
+        index = line.search(/mul\(/);
+        // console.log(index)
+
+        const pos = index + 4;
+        splitline.splice(0,pos)
+        line = line.substring(pos)
+        // console.log(splitline)
+        let valid = true;
+        const c = splitline.findIndex(c => {
+            if (c === ')' ||
+            !(
+                c === ')' ||
+                (c.charCodeAt(0) >= 48 && c.charCodeAt(0) <= 57) ||
+                c.charCodeAt(0) == 44
+            )) {
+                if (c !== ')') {
+                    valid = false;
+                }
+                return true;
+            }
+        });
+        if (c === -1) {
+            valid = false;
+        }
+
+        const result = splitline.splice(0, c)
+        if (valid) {
+             const resultNumbers = result.join('').split(',').map(Number);
+             if (resultNumbers.length === 2) {
+                 totalResult += resultNumbers[0] * resultNumbers[1];
+                 occurences++;
+                 console.log(result.join(''), '=', resultNumbers[0] * resultNumbers[1]);
+             }
+        }
+        line = line.substring(c)
+
+    }
+
+    console.log("Part 1:", totalResult, "Occurences: ", occurences)
+
 }
