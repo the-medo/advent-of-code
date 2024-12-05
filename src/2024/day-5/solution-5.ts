266ba37f4f930d2e4b2bf6f350fa4c1f09a98f72
Medo <martinmederly@gmail.com>
2024-12-05 07:35:52 +0100

    feat: day5 p2

--- src/2024/day-5/solution-5.ts
+++ src/2024/day-5/solution-5.ts
@@ -15,10 +15,11 @@ exports.solution = (input: string[]) => {
     }), {} as D5Rules2)
     const updates: D5Update[] = input.splice(1).map(x => x.split(',').map(Number));
 
-    const checkRulesForUpdate = (rules: D5Rules2, update: D5Update): boolean => {
+    const checkRulesForUpdate = (rules: D5Rules2, update: D5Update): [true, undefined] | [false, [number, number]] => {
         const presence: D5Presence = update.reduce((p, v) => ({...p, [v]: true}), {})
         const includes: Record<number, boolean> = {}
         let correct = true;
+        let brokenRule: [number, number] = [0, 0];
         console.log("===update:", update);
 
         update.forEach(u => {
@@ -34,6 +35,7 @@ exports.solution = (input: string[]) => {
                         if (includes[prev]) {
                             return false;
                         }
+                        brokenRule = [u, prev];
                         return true;
                     }
                     return false;
@@ -44,25 +46,50 @@ exports.solution = (input: string[]) => {
                 }
             }
         })
+        return correct ? [true, undefined] : [false, brokenRule];
+    }
 
-        return correct
+    const fixBrokenRule = (brokenRule: [number, number], update: D5Update): D5Update => {
+        const uIndex = update.findIndex(u => u === brokenRule[0])
+        const prevValue = update.splice(update.findIndex(u => u === brokenRule[1]), 1)[0];
+        console.log({uIndex, prevValue})
+        return [...update.slice(0, uIndex), prevValue, brokenRule[0], ...update.slice(uIndex+1)];
     }
     console.log(rules)
     console.log(updates)
 
 
     let correctCount = 0;
-    let sum = 0;
+    let sumPart1 = 0;
+    let sumPart2 = 0;
     updates.forEach(u => {
         const result = checkRulesForUpdate(rules, u)
         console.log(" ========= ", result, " ========= ")
-        if (result) {
+        if (result[0]) {
             correctCount++;
             console.log("Update: ", u[Math.ceil(u.length / 2)])
-            sum += u[Math.floor(u.length / 2)]
+            sumPart1 += u[Math.floor(u.length / 2)]
+        } else {
+            let isRuleFixed = false;
+            let newRule = [...u];
+            let brokenRule = [...result[1]] as [number, number];
+            let i = 0;
+            while (!isRuleFixed) {
+                i++
+                console.log("New rule: (before) ", newRule)
+                newRule = fixBrokenRule(brokenRule, newRule);
+                console.log("New rule: (after) ", newRule)
+                const f = checkRulesForUpdate(rules, newRule);
+                isRuleFixed = f[0];
+                if (!isRuleFixed) brokenRule = f[1]!;
+                console.log(f);
+            }
+            sumPart2 += newRule[Math.floor(newRule.length / 2)]
+            console.log("Fixed rule:", newRule)
         }
     })
 
-    console.log("Part1: ", correctCount, "sum: ", sum)
+    console.log("Part1: ", correctCount, "sum: ", sumPart1)
+    console.log("Part2: ", sumPart2)
 
 }
