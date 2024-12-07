607e9fa2444d4043221ffb243ac54b83950dd48a
Medo <martinmederly@gmail.com>
2024-12-07 06:29:32 +0100

    feat: day7 p2

--- src/2024/day-7/solution-7.ts
+++ src/2024/day-7/solution-7.ts
@@ -16,22 +16,26 @@ exports.solution = (input: string[]) => {
     })
     lines.forEach(l => {if (maxNums < l.operands.length) maxNums = l.operands.length})
 
+    const finish = (x: D7Equation, position: number, result: number): boolean => {
+        if (result > x.result) return false;
+        if (position === x.operands.length - 1) {
+            return result === x.result;
+        }
+        return solveEquation(x, position + 1, result, '+') || solveEquation(x, position + 1, result, '*') || solveEquation(x, position + 1, result, '||')
+    }
+
     const solveEquation = (x: D7Equation, position: number, resultTillNow: number, nextOperator: string): boolean => {
         if (position < x.operands.length) {
             if (nextOperator === '+') {
-                const result = resultTillNow + x.operands[position];
-                if (result > x.result) return false;
-                if (position === x.operands.length - 1) {
-                    return result === x.result;
-                }
-                return solveEquation(x, position + 1, result, '+') || solveEquation(x, position + 1, result, '*')
+                return finish(x, position, resultTillNow + x.operands[position]);
             } else if (nextOperator === '*') {
                 const result = resultTillNow * x.operands[position];
-                if (result > x.result) return false;
-                if (position === x.operands.length - 1) {
-                    return result === x.result;
-                }
-                return solveEquation(x, position + 1, result, '+') || solveEquation(x, position + 1, result, '*')
+                return finish(x, position, resultTillNow * x.operands[position]);
+            } else if (nextOperator === '||') {
+                let arr = '';
+                for (let i = 0; i < x.operands[position].toString().length; i++) arr += '0';
+                const result = parseInt(resultTillNow.toString() + arr )  + x.operands[position];
+                return finish(x, position, result);
             }
         }
         return false;
@@ -39,11 +43,11 @@ exports.solution = (input: string[]) => {
 
     let sum = 0;
     lines.forEach(l => {
-        l.canBeSolved = solveEquation(l, 1, l.operands[0], '+') || solveEquation(l, 1, l.operands[0], '*');
+        l.canBeSolved = solveEquation(l, 1, l.operands[0], '+') || solveEquation(l, 1, l.operands[0], '*') || solveEquation(l, 1, l.operands[0], '||');
         if (l.canBeSolved) {
             sum += l.result;
         }
     })
 
-    console.log("Part 1: ", sum);
+    console.log("Part 2: ", sum);
 }
