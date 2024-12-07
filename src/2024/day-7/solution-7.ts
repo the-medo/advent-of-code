8279d39b7ad123bf9e6d80fc5c0d61418e4eeb41
Medo <martinmederly@gmail.com>
2024-12-07 06:20:28 +0100

    feat: day7 p1

--- src/2024/day-7/solution-7.ts
+++ src/2024/day-7/solution-7.ts
@@ -1,3 +1,49 @@
+type D7Equation = {
+    result: number;
+    operands: number[];
+    canBeSolved: boolean;
+}
+
 exports.solution = (input: string[]) => {
-    console.log("TODO: solution");
+    let maxNums = 0;
+    const lines: D7Equation[] = input.map(i => {
+        const split = i.split(': ');
+        return {
+            result: parseInt(split[0]),
+            operands: split[1].split(' ').map(Number),
+            canBeSolved: true,
+        }
+    })
+    lines.forEach(l => {if (maxNums < l.operands.length) maxNums = l.operands.length})
+
+    const solveEquation = (x: D7Equation, position: number, resultTillNow: number, nextOperator: string): boolean => {
+        if (position < x.operands.length) {
+            if (nextOperator === '+') {
+                const result = resultTillNow + x.operands[position];
+                if (result > x.result) return false;
+                if (position === x.operands.length - 1) {
+                    return result === x.result;
+                }
+                return solveEquation(x, position + 1, result, '+') || solveEquation(x, position + 1, result, '*')
+            } else if (nextOperator === '*') {
+                const result = resultTillNow * x.operands[position];
+                if (result > x.result) return false;
+                if (position === x.operands.length - 1) {
+                    return result === x.result;
+                }
+                return solveEquation(x, position + 1, result, '+') || solveEquation(x, position + 1, result, '*')
+            }
+        }
+        return false;
+    }
+
+    let sum = 0;
+    lines.forEach(l => {
+        l.canBeSolved = solveEquation(l, 1, l.operands[0], '+') || solveEquation(l, 1, l.operands[0], '*');
+        if (l.canBeSolved) {
+            sum += l.result;
+        }
+    })
+
+    console.log("Part 1: ", sum);
 }
