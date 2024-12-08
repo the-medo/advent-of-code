faf58f0de89b8f974cec7893fae1ad196430204a
Medo <martinmederly@gmail.com>
2024-12-08 17:42:37 +0100

    chore: random tinkering with d7p2

--- src/go/utils/strings_to_ints.go
+++ src/go/utils/strings_to_ints.go
@@ -18,3 +18,17 @@ func StringsToInts(strs []string) []int {
 
 	return ints
 }
+
+func StringsToFloats(strs []string) []float64 {
+	float64s := make([]float64, len(strs))
+
+	for i, s := range strs {
+		val, err := strconv.ParseFloat(strings.TrimSpace(s), 64)
+		if err != nil {
+			panic(err)
+		}
+		float64s[i] = val
+	}
+
+	return float64s
+}
--- src/year2024/day7/day7.go
+++ src/year2024/day7/day7.go
@@ -3,16 +3,17 @@ package day7
 import (
 	"fmt"
 	"github.com/the-medo/advent-of-code/src/go/utils"
+	"math"
 	"strconv"
 	"strings"
 )
 
 type D7Equation struct {
-	result   int
-	operands []int
+	result   float64
+	operands []float64
 }
 
-func (e *D7Equation) finish(position int, result int) bool {
+func (e *D7Equation) finish(position int, result float64) bool {
 	if result > e.result {
 		return false
 	}
@@ -24,7 +25,7 @@ func (e *D7Equation) finish(position int, result int) bool {
 		e.solve(position+1, result, "||")
 }
 
-func (e *D7Equation) solve(position int, resultTillNow int, nextOperator string) bool {
+func (e *D7Equation) solve(position int, resultTillNow float64, nextOperator string) bool {
 	if position >= len(e.operands) {
 		return false
 	}
@@ -34,13 +35,16 @@ func (e *D7Equation) solve(position int, resultTillNow int, nextOperator string)
 	case "*":
 		return e.finish(position, resultTillNow*e.operands[position])
 	case "||":
+		// VERSION 4 = full float math.Log => cca 45ms
+		newNumber := resultTillNow*math.Pow(10, math.Floor(math.Log10(e.operands[position])+1.0)) + e.operands[position]
+
 		// VERSION 3 = small integer loop => cca 23ms
-		temp := e.operands[position]
-		for temp > 0 {
-			resultTillNow *= 10
-			temp /= 10
-		}
-		newNumber := resultTillNow + e.operands[position]
+		//temp := e.operands[position]
+		//for temp > 0 {
+		//	resultTillNow *= 10
+		//	temp /= 10
+		//}
+		//newNumber := resultTillNow + e.operands[position]
 
 		// VERSION 2 = math.Log => cca 40ms
 		//newNumber := int(float64(resultTillNow)*math.Pow10(int(math.Floor(math.Log10(float64(e.operands[position]))+1)))) + e.operands[position]
@@ -59,15 +63,15 @@ func Solve(input string) {
 	equations := make([]D7Equation, len(rows))
 	for i, row := range rows {
 		equationParts := strings.Split(row, ": ")
-		equationResult, _ := strconv.Atoi(equationParts[0])
-		operands := utils.StringsToInts(strings.Split(equationParts[1], " "))
+		equationResult, _ := strconv.ParseFloat(equationParts[0], 64)
+		operands := utils.StringsToFloats(strings.Split(equationParts[1], " "))
 		equations[i] = D7Equation{
 			result:   equationResult,
 			operands: operands,
 		}
 	}
 
-	sum := 0
+	sum := float64(0)
 	for i := range equations {
 		eq := &equations[i]
 		canBeSolved := eq.solve(1, eq.operands[0], "+") ||
