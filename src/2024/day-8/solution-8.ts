a9df02f1eaa1a4dad661e92327b6d35f6773958c
Medo <martinmederly@gmail.com>
2024-12-08 07:05:54 +0100

    feat: day8 refactor

--- src/2024/day-8/solution-8.ts
+++ src/2024/day-8/solution-8.ts
@@ -1,26 +1,20 @@
 type D8Point = {
     x: number;
     y: number;
-    type?: string;
 }
 
 type D8AntennaMap = Record<string, D8Point[] | undefined>
-type D8AntinodeMap = Record<string, string[] | undefined>
+type D8AntinodeMap = Record<string, true | undefined>
 
 exports.solution = (input: string[]) => {
-    const aMap: D8AntennaMap = {};
-    const antinodeMap: D8AntinodeMap = {};
+    const antennaPoints: D8AntennaMap = {};
     let height = input.length, width = input[0].length;
 
     input.forEach((row, y) => {
         row.split('').forEach((c, x) => {
             if (c !== '.') {
-                if (!aMap[c]) aMap[c] = [];
-                aMap[c]?.push({
-                    x,
-                    y,
-                    type: c
-                })
+                if (!antennaPoints[c]) antennaPoints[c] = [];
+                antennaPoints[c]?.push({x,y})
             }
         })
     })
@@ -28,47 +22,39 @@ exports.solution = (input: string[]) => {
     const isOutOfBounds = (p: D8Point) => p.x < 0 || p.x >= width || p.y < 0 || p.y >= height;
     const getKey = (p: D8Point) => `${p.x};${p.y}`;
 
-    const antenas = Object.keys(aMap);
+    const getPointsInLine = (point: D8Point, xDiff: number, yDiff: number, part: number): D8Point[] => {
+        const result: D8Point[] = [];
 
-    antenas.forEach(a => {
-        aMap[a]?.forEach(p1 => {
-            aMap[a]?.forEach(p2 => {
-                if (p1.x === p2.x && p1.y === p2.y) return;
-                const xDiff = p1.x - p2.x;
-                const yDiff = p1.y - p2.y;
+        [1, -1].forEach(multiplier => {
+            let p = {...point};
+            while (true) {
+                p.x += xDiff * multiplier;
+                p.y += yDiff * multiplier;
+                if (isOutOfBounds(p)) break;
+                result.push({...p})
+                if (part === 1) break;
+            }
+        })
 
-                const antiP = [];
+        return result;
+    }
 
-                let p = {x: p1.x, y: p1.y};
-                while (!isOutOfBounds(p)) {
-                    p.x -= xDiff;
-                    p.y -= yDiff;
-                    antiP.push({...p})
-                    console.log("While - ", p)
-                }
-                p = {x: p1.x, y: p1.y};
-                while (!isOutOfBounds(p)) {
-                    p.x += xDiff;
-                    p.y += yDiff;
-                    antiP.push({...p})
-                    console.log("While + ", p)
-                }
-                console.log(antiP);
-
-                const antinodes = antiP;//.filter(p => !((p.x === p1.x && p.y === p1.y) || (p.x === p2.x && p.y === p2.y)));
-
-                // console.log(antinodes);
-                antinodes.forEach(p => {
-                    if (!isOutOfBounds(p)) {
-                        antinodeMap[getKey(p)] = [a];
-                    }
+    const run = (part: number): void => {
+        const antinodeMap: D8AntinodeMap = {};
+        Object.keys(antennaPoints).forEach(a => {
+            antennaPoints[a]?.forEach(p1 => {
+                antennaPoints[a]?.forEach(p2 => {
+                    if (p1.x === p2.x && p1.y === p2.y) return;
+                    let antinodes = getPointsInLine(p1, p1.x - p2.x, p1.y - p2.y, part)
+                    if (part === 1) antinodes = antinodes.filter(p => !((p.x === p1.x && p.y === p1.y) || (p.x === p2.x && p.y === p2.y)));
+                    antinodes.forEach(p => antinodeMap[getKey(p)] = true)
                 })
             })
         })
-    })
 
-    console.log(antinodeMap);
+        console.log(`Part ${part}: `, Object.keys(antinodeMap).length);
+    }
 
-
-    console.log("Part 2: ", Object.keys(antinodeMap).length);
+    run(1);
+    run(2);
 }
