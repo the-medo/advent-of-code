a53b8955869d0b985a01b760d97e831876d441e5
Medo <martinmederly@gmail.com>
2024-12-09 06:56:51 +0100

    feat: d9p2

--- src/year2024/day9/solution-9.ts
+++ src/year2024/day9/solution-9.ts
@@ -21,36 +21,37 @@ exports.solution = (input: string[]) => {
     console.log(result);
     let nearestSpaceId = 1;
     for (let i = result.length - 1; i >= 0; i--) {
+        nearestSpaceId = 1;
         if (nearestSpaceId > i) break;
         if (result[i].type === 'file') {
             const file = result[i];
-            console.log("File on ", i, file);
+            // console.log("File on ", i, file);
             let remainingSize = file.size;
             while (remainingSize > 0 && nearestSpaceId < i) {
-                console.log("== ", result[nearestSpaceId])
+                // console.log("== ", result[nearestSpaceId])
                 if (result[nearestSpaceId].type === 'space') {
                     const space = result[nearestSpaceId];
                     if (space.size > remainingSize) {
-                        console.log("== >", space.size, remainingSize)
+                        // console.log("== >", space.size, remainingSize)
                         space.size -= remainingSize;
                         result = [...result.splice(0, nearestSpaceId), {id: file.id, size: remainingSize, type: 'file'}, ...result.splice(0)]
                         file.id = 0;
                         file.type = 'space';
                         remainingSize = 0;
                     } else if (space.size === remainingSize) {
-                        console.log("== ==", space.size, remainingSize)
+                        // console.log("== ==", space.size, remainingSize)
                         remainingSize = 0;
                         space.id = file.id;
                         space.type = 'file';
                         file.id = 0;
                         file.type = 'space';
-                    } else {
+                    } /*else {
                         console.log("== <", space.size, remainingSize)
                         remainingSize -= space.size;
                         space.type = file.type;
                         space.id = file.id;
                         file.size -= space.size
-                    }
+                    }*/
                 }
                 nearestSpaceId++;
             }
@@ -64,9 +65,10 @@ exports.solution = (input: string[]) => {
             for (let l = i; l < i+f.size; l++) {
                 sum += f.id * l
             }
-            i+=f.size;
         }
+        i+=f.size;
     })
 
     console.log(sum);
+    console.log(result);
 }
