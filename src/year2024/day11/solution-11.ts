2029728bfbc1fbbfdef0b403163af33e423bd04e
Medo <martinmederly@gmail.com>
2024-12-12 06:24:19 +0100

    feat: d12p1

--- src/year2024/day11/solution-11.ts
+++ src/year2024/day11/solution-11.ts
@@ -5,6 +5,7 @@ type D11Type = {
 
 exports.solution = (input: string[]) => {
     const run = (part: number ) => {
+        const t0 = performance.now();
         let stones = input[0].split(' ').map(Number);
 
         const after5Blinks: Record<number, number[]> = {}
@@ -34,17 +35,13 @@ exports.solution = (input: string[]) => {
             return stones;
         }
 
-
         stones.forEach(s => {
             if (!after5Blinks[s]) {
                 after5Blinks[s] = blink5(s);
             }
         })
 
-
-
         for (let blinks = 1; blinks < blinkLimit; blinks += 5) {
-            console.log("Step", blinks)
             Object.keys(after5Blinks).map(Number).forEach(k => {
                 after5Blinks[k].forEach(s => {
                     if (!after5Blinks[s]) {
@@ -56,11 +53,7 @@ exports.solution = (input: string[]) => {
 
         const keyCounts: Record<number, number> = stones.reduce((p, c) => ({...p, [c]: (p[c] ?? 0) + 1}), {} as Record<number, number>);
 
-        console.log(keyCounts)
-
         for (let blinks = 0; blinks < blinkLimit; blinks += 5) {
-            console.log("======")
-            console.log("Step", blinks)
             const copy = {...keyCounts}
             Object.keys(keyCounts).map(Number).forEach(stone => {
                 const count = copy[stone];
@@ -74,15 +67,13 @@ exports.solution = (input: string[]) => {
                 })
                 keyCounts[stone]-=count;
             })
-            console.log(keyCounts)
-            console.log("======")
         }
 
-        console.log("======")
-        // console.log(keyCounts)
         let sum = 0;
         Object.keys(keyCounts).forEach(k => sum += keyCounts[parseInt(k)]);
         console.log(`Part ${part}: ${sum}`)
+        const t1 = performance.now();
+        console.log(`Execution time: ${t1 - t0} milliseconds.`);
     }
 
     run(1)
--- src/year2024/day12/solution-12.ts
+++ src/year2024/day12/solution-12.ts
@@ -1,3 +1,83 @@
+type D12Point = {
+    x: number;
+    y: number;
+    c: string;
+    neighborsOfSameType: D12Point[];
+    plotted: boolean;
+}
+type D12Map = D12Point[][]
+
+type D12Plot = {
+    c: string;
+    points: D12Point[];
+    area: number;
+    perimeter: number;
+}
+
+
+
 exports.solution = (input: string[]) => {
-    console.log("TODO: solution");
+    const height = input.length, width = input[0].length;
+    const isOutOfBoundsXY = (x: number, y: number) => x < 0 || x >= width || y < 0 || y >= height;
+    const getKey = (p: D12Point) => `${p.x};${p.y}`;
+    const m: D12Map = [];
+    const plots: D12Plot[] = [];
+
+    const addNeighbors = (p1: D12Point, p2: D12Point) => {
+        if (p1.c === p2.c) {
+            p1.neighborsOfSameType.push(p2);
+            p2.neighborsOfSameType.push(p1);
+        }
+    }
+
+    input.forEach((row, y) => {
+        row.split('').forEach((c, x) => {
+            if (!m[x]) m[x] = [];
+            m[x][y] = { x, y, c, neighborsOfSameType: [], plotted: false };
+            if (!isOutOfBoundsXY(x-1, y)) addNeighbors(m[x][y], m[x-1][y])
+            if (!isOutOfBoundsXY(x, y-1)) addNeighbors(m[x][y], m[x][y-1])
+        })
+    })
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            const point = m[x][y];
+            if (!point.plotted) {
+                const newPlot: D12Plot = {
+                    c: point.c,
+                    points: [],
+                    area: 0,
+                    perimeter: 0,
+                }
+
+                const stack = [point]
+                while (stack.length > 0) {
+                    const p = stack.pop();
+                    if (p && !p.plotted) {
+                        p.plotted = true;
+                        newPlot.points.push(p)
+                        newPlot.area++;
+                        newPlot.perimeter += (4 - p.neighborsOfSameType.length);
+                        p.neighborsOfSameType.forEach(n => {
+                            if (!n.plotted && n.c === point.c) {
+                                stack.push(n)
+                            }
+                        })
+                    }
+                }
+
+                plots.push(newPlot);
+            }
+        }
+    }
+
+    let sum = 0;
+    plots.forEach(p => {
+        sum += p.area * p.perimeter
+    })
+
+    console.log(plots);
+    console.log(m[1][1].neighborsOfSameType);
+    console.log("Part 1: ", sum)
+
 }
