d25a72736ac5c952993ff9da4f5212063a40ecd0
Medo <martinmederly@gmail.com>
2024-12-18 08:37:03 +0100

    feat: d18 refactor

--- src/year2024/day18/solution-18.ts
+++ src/year2024/day18/solution-18.ts
@@ -1,16 +1,14 @@
 import { DoublyLinkedList } from "../../ts/utils/DoublyLinkedList";
 
 type D18VisitedMap = Record<string, boolean>;
-type D18PqStep = {
+type D18Step = {
     x: number;
     y: number;
     steps: number;
-
 }
 
 exports.solution = (input: string[]) => {
-    // const width = 7, height = 7, start = "0,0", finish = "6,6", corruptedLimit = 12;
-    const width = 71, height = 71, start = "0,0", finish = "70,70", corruptedLimit = 2024;
+    const width = 71, height = 71, start = "0,0", finish = "70,70";
 
     const isOutOfBoundsXY = (x: number, y: number) => x < 0 || x >= width || y < 0 || y >= height;
     const getKey = (x: number, y: number) => `${x},${y}`;
@@ -18,8 +16,9 @@ exports.solution = (input: string[]) => {
 
     const runWithCorruptedLimit = (corruptedLimit: number): number => {
         let corrupted: D18VisitedMap = {};
-        let shortestMap: Record<string, number> = {};
-        shortestMap["0,0"] = 0;
+        let shortestMap: Record<string, number> = {
+            "0,0": 0,
+        };
 
         input.forEach((key, i) => {
             if (i >= corruptedLimit) return;
@@ -28,53 +27,37 @@ exports.solution = (input: string[]) => {
 
         let shortestPath = Infinity;
 
-        const move = (step: D18PqStep): D18PqStep[] => {
-            const skey = getKey(step.x, step.y);
-            if (skey === finish) {
-                if (shortestPath > step.steps) {
-                    shortestPath = step.steps;
-                }
+        const move = (step: D18Step): D18Step[] => {
+            const sKey = getKey(step.x, step.y);
+            if (sKey === finish) {
+                if (shortestPath > step.steps) shortestPath = step.steps;
                 return []
-            } else if (step.steps > shortestPath) {
-                return []
-            } else {
-                if (shortestMap[skey]) {
-                    if (shortestMap[skey] < step.steps) {
-                        return [];
-                    }
-                }
-                shortestMap[skey] = step.steps;
             }
+            if (step.steps > shortestPath) return [];
+            shortestMap[sKey] = step.steps;
 
-            const newPositions: [number, number][] = [];
+            const newSteps: D18Step[] = [];
 
             directions.forEach(d => {
                 const newX = step.x + d[0], newY = step.y + d[1];
-                if (!isOutOfBoundsXY(newX, newY)) {
-                    const key = getKey(newX, newY);
-                    const sp = shortestMap[key];
-                    if (!corrupted[key] && (!sp || sp > step.steps + 1)) newPositions.push([newX, newY]);
-                }
-            })
+                if (isOutOfBoundsXY(newX, newY)) return;
+                const key = getKey(newX, newY);
+                const sp = shortestMap[key];
+                if (!corrupted[key] && (!sp || sp > step.steps + 1)) newSteps.push({
+                    x: newX,
+                    y: newY,
+                    steps: step.steps + 1
+                });
+            });
 
-            return newPositions.map(np => ({
-                x: np[0],
-                y: np[1],
-                steps: step.steps + 1
-            }))
+            return newSteps;
         }
 
-        let i = 1;
-        const stack = new DoublyLinkedList<D18PqStep>()
+        const stack = new DoublyLinkedList<D18Step>()
         stack.push({x: 0, y: 0, steps: 0})
         while (stack.length > 0) {
             const step = stack.pop();
-            if (step) {
-                // if (i % 100000 === 0) console.log(`${i} - stack: ${stack.length}`)
-                const newSteps = move(step.val);
-                newSteps.forEach(s => stack.push(s))
-                i++
-            }
+            if (step) move(step.val).forEach(s => stack.push(s))
         }
 
         return shortestPath;
@@ -85,27 +68,30 @@ exports.solution = (input: string[]) => {
      * Part 2 returns index of byte that destroys last possible path
      * @param part
      */
-    const run = (part: number): number => {
-        if (part === 1) return runWithCorruptedLimit(1024);
-        let lowerLimit = 1025;
-        let upperLimit = 3450;
+    const run = (part: number) => {
+        const t0 = performance.now();
+        if (part === 1) console.log("Part 1: ", runWithCorruptedLimit(1024));
+        if (part === 2) {
+            let lowerLimit = 1025;
+            let upperLimit = 3450;
 
-        //we loop till we get the breaking point between Infinity (not possible) and normal number (possible)
-        while (lowerLimit < upperLimit) {
-            const corruptedLimit = lowerLimit + Math.ceil((upperLimit - lowerLimit) / 2)
-            console.log("Running with: ", {corruptedLimit, lowerLimit, upperLimit})
-            const result = runWithCorruptedLimit(corruptedLimit)
-            if (result === Infinity) {
-                upperLimit = corruptedLimit - 1;
-            } else {
-                lowerLimit = corruptedLimit + 1;
+            //we loop till we get the breaking point between Infinity (not possible) and normal number (possible)
+            while (lowerLimit < upperLimit) {
+                const corruptedLimit = lowerLimit + Math.ceil((upperLimit - lowerLimit) / 2)
+                // console.log("Running with: ", {corruptedLimit, lowerLimit, upperLimit})
+                const result = runWithCorruptedLimit(corruptedLimit)
+                if (result === Infinity) {
+                    upperLimit = corruptedLimit - 1;
+                } else {
+                    lowerLimit = corruptedLimit + 1;
+                }
             }
+            console.log("Part 2: ", input[lowerLimit]);
         }
-
-        return lowerLimit;
+        const t1 = performance.now();
+        console.log(`Execution time: ${t1 - t0} milliseconds.`);
     }
 
-    console.log("Part 1: ", run(1) )
-    console.log("Part 2: ", input[run(2)])
-
+    run(1);
+    run(2);
 }
