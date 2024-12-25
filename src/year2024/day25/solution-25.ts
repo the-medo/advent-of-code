124147d0dde475277d2d2eae4fa6b6a2774a7aa0
Medo <martinmederly@gmail.com>
2024-12-25 06:31:35 +0100

    feat: d25p1 cleaned up

--- src/year2024/day25/solution-25.ts
+++ src/year2024/day25/solution-25.ts
@@ -1,3 +1,33 @@
 exports.solution = (input: string[]) => {
-    console.log("TODO: solution");
+    const keys: number[][] = [];
+    const locks: number[][] = [];
+    let lastRowEmpty = true;
+    let currentKeyOrLock: number[] = [0,0,0,0,0]
+    input.forEach(row => {
+        if (lastRowEmpty) {
+            currentKeyOrLock = [0,0,0,0,0];
+            (row[0] === '#' ? locks : keys).push(currentKeyOrLock);
+            lastRowEmpty = false;
+        }
+        if (row === '') lastRowEmpty = true;
+        row.split('').forEach((c, i) => {
+            if (c === '#') currentKeyOrLock[i]++;
+        })
+    })
+
+    let combinations = 0;
+    locks.forEach((lock) => {
+        keys.forEach((key) => {
+            let validKey = true;
+            for (let i = 0; i < 5; i++) {
+                if (lock[i] + key[i] > 7) {
+                    validKey = false;
+                    break;
+                }
+            }
+            if (validKey) combinations++
+        })
+    })
+
+    console.log("Part 1: ", combinations)
 }
