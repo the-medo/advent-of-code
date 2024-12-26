c4f36da47c79960b71c1e5f23849fee910e35d10
Medo <martinmederly@gmail.com>
2024-12-26 22:55:01 +0100

    feat: y2021 d17 p1

--- src/year2021/day17/solution-17.ts
+++ src/year2021/day17/solution-17.ts
@@ -1,8 +1,15 @@
 exports.solution = (input: string[]) => {
     const t0 = performance.now();
-    
-    console.log("TODO: solution");
-    
+    const [[xFrom, xTo], [yFrom, yTo]]= input[0].split(": ")[1].split(", ").map(a => a.split("=")[1].split("..").map(Number));
+
+    let maxY = 0;
+    const startVelocity = 0 - yFrom - 1;
+    for (let i = startVelocity; i >= 0; i--) {
+        maxY += i;
+    }
+
+    console.log("Part 1: ", maxY)
+
     const t1 = performance.now();
     console.log(`Execution time: ${t1 - t0} milliseconds.`);
 }
