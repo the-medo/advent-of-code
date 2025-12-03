80632df8b4a9c812009e444d6d565e8ce79b4362
Medo <martinmederly@gmail.com>
2025-12-03 06:48:15 +0100

    feat: y2025d3 cleanup

--- src/year2025/day3/solution-3.ts
+++ src/year2025/day3/solution-3.ts
@@ -21,26 +21,25 @@ const computeJoltage = (digits: number, numbers: number[]): D3Joltages => {
     return [max, ...computeJoltage(digits - 1, numbers.slice(resultingIndex + 1))]
 }
 
-exports.solution = (input: string[]) => {
-    const t0 = performance.now();
+const computeFinalJoltage = (joltages: D3Joltages[]): number => {
+    return joltages.map(j => parseInt(j.join(''))).reduce((acc, curr) => acc + curr, 0);
+}
 
-    let joltages1: D3Joltages[] = [];
-    let joltages2: D3Joltages[] = [];
+const computePart = (part: number, input: string[], digits: number) => {
+    const t0 = performance.now();
+    let joltages: D3Joltages[] = [];
     input.forEach(row => {
         const numbers = row.split('').map(Number);
-        const joltage1 = computeJoltage(2, numbers)
-        const joltage2 = computeJoltage(12, numbers)
-        joltages1.push(joltage1);
-        joltages2.push(joltage2);
-        console.log("Row: ", row, "J:", joltage1)
-    })
+        const joltage1 = computeJoltage(digits, numbers);
+        joltages.push(joltage1);
+    });
 
-    const part1 = joltages1.map(j => parseInt(j.join(''))).reduce((acc, curr) => acc + curr, 0);
-    const part2 = joltages2.map(j => parseInt(j.join(''))).reduce((acc, curr) => acc + curr, 0);
-
-    console.log(`Part 1: ${part1}`)
-    console.log(`Part 2: ${part2}`)
-
+    const result = computeFinalJoltage(joltages);
     const t1 = performance.now();
-    console.log(`Execution time: ${t1 - t0} milliseconds.`);
+    console.log(`Part ${part}: ${result} [${t1 - t0} ms]`)
+}
+
+exports.solution = (input: string[]) => {
+    computePart(1, input, 2);
+    computePart(2, input, 12);
 }
