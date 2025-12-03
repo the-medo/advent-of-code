b37824f4873481e3078f42c3e1944067bfb57241
Medo <martinmederly@gmail.com>
2025-12-03 06:28:48 +0100

    feat: y2025d3 p2

--- src/year2025/day3/solution-3.ts
+++ src/year2025/day3/solution-3.ts
@@ -4,35 +4,42 @@ type D3Joltage = {
     n2: number;
 }
 
+type D3Joltages = number[];
+
+const computeJoltage = (digits: number, numbers: number[]): D3Joltages => {
+    if (digits === 0) return [];
+    let max = numbers[0]
+    let resultingIndex = 0;
+    numbers.forEach((n, i) => {
+        if (i > numbers.length - digits) return;
+        if (n > max) {
+            max = n;
+            resultingIndex = i;
+        }
+    })
+
+    return [max, ...computeJoltage(digits - 1, numbers.slice(resultingIndex + 1))]
+}
+
 exports.solution = (input: string[]) => {
     const t0 = performance.now();
 
-    let joltages: D3Joltage[] = [];
+    let joltages1: D3Joltages[] = [];
+    let joltages2: D3Joltages[] = [];
     input.forEach(row => {
         const numbers = row.split('').map(Number);
-        let n1 = numbers[0], n2 = numbers[1];
-
-        numbers.forEach((n, i) => {
-            if (i < 1) return;
-            if (i === numbers.length -1) {
-                if (n > n2) n2 = n;
-                return;
-            }
-            if (n > n1) {
-                n1 = n;
-                n2 = numbers[i+1];
-            } else if (n > n2) {
-                n2 = n;
-            }
-        });
-        const joltage = {n1, n2};
-        joltages.push(joltage);
-        console.log("Row: ", row, "J:", joltage)
+        const joltage1 = computeJoltage(2, numbers)
+        const joltage2 = computeJoltage(12, numbers)
+        joltages1.push(joltage1);
+        joltages2.push(joltage2);
+        console.log("Row: ", row, "J:", joltage1)
     })
 
-    const part1 = joltages.map(j => j.n1 * 10 + j.n2).reduce((acc, curr) => acc + curr, 0);
+    const part1 = joltages1.map(j => parseInt(j.join(''))).reduce((acc, curr) => acc + curr, 0);
+    const part2 = joltages2.map(j => parseInt(j.join(''))).reduce((acc, curr) => acc + curr, 0);
 
     console.log(`Part 1: ${part1}`)
+    console.log(`Part 2: ${part2}`)
 
     const t1 = performance.now();
     console.log(`Execution time: ${t1 - t0} milliseconds.`);
