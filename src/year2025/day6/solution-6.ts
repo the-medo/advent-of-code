37d61065cabeeb8865b6c1fc0dacfd6a361fdb6b
Medo <martinmederly@gmail.com>
2025-12-06 06:37:12 +0100

    feat: y2025d6 p2

--- src/year2025/day6/solution-6.ts
+++ src/year2025/day6/solution-6.ts
@@ -4,9 +4,24 @@ type D6Math = {
     result: number;
 }
 
-exports.solution = (input: string[]) => {
-    const t0 = performance.now();
+const computeD6MathResult = (m: D6Math) => {
+    m.result = m.parts.reduce((acc, curr) =>
+        m.action === '*'
+            ? acc * curr
+            : acc + curr,
+        m.action === '*'
+            ? 1
+            : 0
+    )
+}
 
+const createD6Math = (): D6Math => ({
+    parts: [],
+    action: '',
+    result: 0,
+})
+
+const d6Part1 = (input: string[]) => {
     const maths: D6Math[] = [];
     let totalSum = 0;
     input.forEach((row, rowNumber) => {
@@ -14,22 +29,11 @@ exports.solution = (input: string[]) => {
         const mathProblemParts = trimmedRow.split(' ');
         mathProblemParts.forEach((part, colNumber) => {
             if (!maths[colNumber]) {
-                maths.push({
-                    parts: [],
-                    action: '',
-                    result: 0,
-                })
+                maths.push(createD6Math())
             }
             if (rowNumber === input.length - 1) {
                 maths[colNumber].action = part;
-                maths[colNumber].result = maths[colNumber].parts.reduce((acc, curr) =>
-                    maths[colNumber].action === '*'
-                        ? acc * curr
-                        : acc + curr,
-                    maths[colNumber].action === '*'
-                        ? 1
-                        : 0
-                )
+                computeD6MathResult(maths[colNumber]);
                 totalSum += maths[colNumber].result;
                 console.log(maths[colNumber])
             } else {
@@ -37,9 +41,52 @@ exports.solution = (input: string[]) => {
             }
         })
     })
-    
+
     console.log("Part 1: ", totalSum);
-    
+}
+
+const d6Part2 = (input: string[]) => {
+    const maxCol = Math.max(...input.map(row => row.length)) - 1
+
+    const maths: D6Math[] = [];
+    let totalSum = 0;
+    let currentMath = createD6Math();
+
+    for (let colNumber = maxCol; colNumber >= 0; colNumber--) {
+        let stringifiedNumber = '';
+        let willBeNewMath = false;
+        input.forEach((row, rowNumber) => {
+            if (rowNumber === input.length - 1) {
+                if (row[colNumber] === '*' || row[colNumber] === '+') {
+                    currentMath.action = row[colNumber];
+                    willBeNewMath = true; // action is always in the first column
+                }
+            } else {
+                stringifiedNumber = `${stringifiedNumber}${row[colNumber] ?? ' '}`;
+            }
+        })
+        currentMath.parts.push(Number(stringifiedNumber));
+
+        if (willBeNewMath) {
+            computeD6MathResult(currentMath);
+            totalSum += currentMath.result;
+            console.log(currentMath)
+            colNumber--; // next column are just spaces
+            maths.push({...currentMath})
+            currentMath = createD6Math();
+        }
+
+    }
+
+    console.log("Part 2: ", totalSum);
+}
+
+exports.solution = (input: string[]) => {
+    const t0 = performance.now();
+
+    d6Part1(input);
+    d6Part2(input);
+
     const t1 = performance.now();
     console.log(`Execution time: ${t1 - t0} milliseconds.`);
 }
